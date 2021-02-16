5b60d885844e151a1e00e6aa1751a528eadd922d
sandeep-6698 <sandeep.kumar@taknikbharti.com>
2021-02-16 11:26:45 +0530

    initial push

---
+++ public/js/script.js
@@ -0,0 +1,19 @@
+$("form").on("submit", (e) => {
+  alert("Mail has been sent successfully");
+  //   let name = $("#name").val();
+  //   let email = $("#email").val();
+  //   let message = $("#message").val();
+  //   let mobile = $("#mobile").val();
+  //   e.preventDefault();
+  //   Email.send({
+  //     Host: "smtp.gmail.com",
+  //     Username: "otheruse95096@gmail.com",
+  //     Password: "9509624303",
+  //     To: "otheruse95096@gmail.com",
+  //     From: email.value,
+  //     Subject: "Get in touch with " + name,
+  //     Body: message.value,
+  //   }).then(function (message) {
+  //     alert("Mail has been sent successfully");
+  //   });
+});
